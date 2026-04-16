edb849525c9e6b271896da91875b3eba69dbcc16
MarleMGN <m0b.torres23@gmail.com>
2026-04-15 18:42:43 -0700

    URLSearchParams fix

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -10,37 +10,32 @@ const Modal = ({ closeModal, openModal }) => {
   const [mensagem, setMensagem] = useState("");
 
   function submitForm() {
-    if (!nome.trim() || !presenca) {
-      alert("Por favor, preencha seu nome e confirmação de presença.");
-      return;
-    }
-    const formUrl =
-      "https://script.google.com/macros/s/AKfycbzmwtDMjI2Rgh6Zh1WheMglryPE-XW3lXAoQoTJAbQQRoEQqfXzOVIkZF6BxPdG1NwYxg/exec";
-
-    // const data = new FormData();
-    // data.append("entry.949743175", nome);
-    // data.append("entry.1507187331", presenca);
-    // data.append("entry.1979707589", email);
-
-    // data.append("entry.877214974", telefone);
-    // data.append("entry.679812480", convidados);
-    // data.append("entry.303326317", mensagem);
-
-    fetch(formUrl, {
-      method: "POST",
-      mode: "no-cors",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams({
-        nome,
-        presenca,
-        email,
-        telefone,
-        convidados,
-        mensagem,
-      }),
-    });
-    setSubmitted(true);
+  if (!nome.trim() || !presenca) {
+    alert("Por favor, preencha seu nome e confirmação de presença.");
+    return;
   }
+
+  const formUrl =
+    "https://script.google.com/macros/s/AKfycbzmwtDMjI2Rgh6Zh1WheMglryPE-XW3lXAoQoTJAbQQRoEQqfXzOVIkZF6BxPdG1NwYxg/exec";
+
+  fetch(formUrl, {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      "entry.949743175": nome,
+      "entry.1979707589": email,
+      "entry.877214974": telefone,
+      "entry.1507187331": presenca,
+      "entry.679812480": convidados,
+      "entry.303326317": mensagem,
+    }),
+  });
+
+  setSubmitted(true);
+}
 
   return (
     <div
